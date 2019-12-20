517c6bc31e33875f058e3aad4b1e98420db5afa5
18735687236 <246730576@qq.com>
2019-12-20 12:01:35 +0800

    12-20-1

--- src/store/modules/examination.js
+++ src/store/modules/examination.js
@@ -1,15 +1,79 @@
-
+import {getExam} from "@/api/examination.js"
 const state = {
-
+    data:{
+        subject_id:"",
+        exam_id:"",
+        title:"",
+        number:4,
+        start_time:"",
+        end_time:""
+    },
+    types: [{
+        value: '选项1',
+        label: '周考一'
+      }, {
+        value: '选项2',
+        label: '周考二'
+      }, {
+        value: '选项3',
+        label: '周考三'
+      }, {
+        value: '选项4',
+        label: '月考'
+      }],
+      course:[{
+        typevalue: '选项1',
+        label: 'javaScript上'
+    },
+    {
+        typevalue: '选项2',
+        label: 'javaScript下'
+    },
+    {
+        typevalue: '选项3',
+        label: '模块化开发'
+    },
+    {
+        typevalue: '选项4',
+        label: '移动端开发'
+    },
+    {
+        typevalue: '选项5',
+        label: 'node基础'
+    },
+    {
+        typevalue: '选项6',
+        label: '组件式开发'
+    },
+    {
+        typevalue: '选项7',
+        label: '渐进式开发'
+    },
+    {
+        typevalue: '选项8',
+        label: '项目实战'
+    },
+    {
+        typevalue: '选项9',
+        label: 'javaScript高级'
+    }]
 }
 
 const mutations = {
+    updateExam(state,payload){
+      state.data=payload
+    },
+    setSubject(state,payload){
 
+    }
 }
 
 const actions = {
-   getExam({commit},payload){
+   async getExam({commit},payload){
        window.console.log(payload)
+       let res = await getExam(payload)
+       window.console.log(res)
+       commit("updateExam",res.data)
    }
 }
 
--- src/views/examination/addTest.vue
+++ src/views/examination/addTest.vue
@@ -6,112 +6,136 @@
             </el-header>
             <el-main>
                 <div class="ant-layout-content">
-                    <div class="ant-form">
-                        <label for="title" class="ant-form-item-required" title="试卷名称">试卷名称:</label>
-                    </div>
-                    <div class="ant-form-item-control">
-                        <span class="ant-form-item-children">
-                            <input type="text" id="title" class="ant-input">
-                        </span>
-                    </div>
-                    <div class="ant-form">
-                        <label for="title" class="ant-form-item-required" title="试卷名称">选择考试类型:</label>
-                    </div>
-                     <div class="ant-form-item-control">
-                        <span class="ant-form-item-children">
-                            <input type="text" id="title" class="ant-input">
-                        </span>
-                    </div>
-                    <!-- <div class="ant-select">
-                        <div class="ant-select-selection">
-                            <input type="text">
-                            <div class="ant-select-selection__rendered"></div>
-                            <span class="ant-select-arrow">
-                                <i aria-label="图标: down" class="ant-select-arrow-icon">
-                                    <svg></svg>
-                                </i>
-                            </span>
+                    <div class="ant-row">
+                        <div class="ant-form-item">
+                            <div class="ant-form-item-label">
+                                <label for="title" class="ant-form-item-required">试卷名称:</label>
+                            </div>
+                            <input type="text" id="title" class="ant-input" v-model="subject_id">                            
                         </div>
-                    </div> -->
-                    <div class="ant-form">
-                        <label for="title" class="ant-form-item-required" title="试卷名称">选择课程:</label>
-                    </div>
-                     <div class="ant-form-item-control">
-                        <span class="ant-form-item-children">
-                            <input type="text" id="title" class="ant-input">
-                        </span>
-                    </div>
-                    <!-- <div class="ant-select">
-                        <div class="ant-select-selection">
-                            <div class="ant-select-selection__rendered"></div>
-                            <span class="ant-select-arrow">
-                                <i aria-label="图标: down" class="ant-select-arrow-icon">
-                                    <svg></svg>
-                                </i>
-                            </span>
+                        <div class="ant-form-item">
+                            <div class="ant-form-item-label">
+                                <label for="title" class="ant-form-item-required">选择试卷类型:</label>
+                            </div>
+                            <div class="ant-form-item-control">
+                                <el-select class="ant-select-selection-single" v-model="value" placeholder="">
+                                    <el-option 
+                                        v-for="item in types"
+                                        :key="item.value"
+                                        :label="item.label"
+                                        :value="item.value"
+                                    ></el-option>
+                                </el-select>
+                            </div>
                         </div>
-                    </div> -->
-                    <div class="ant-form">
-                        <label for="title" class="ant-form-item-required" title="试卷名称">设置题量:</label>
-                    </div>
-                     <div class="ant-form-item-control">
-                        <span class="ant-form-item-children">
-                            <input type="text" id="title" class="ant-input">
-                        </span>
-                    </div>
-                    <div class="ant-form">
-                        <label for="title" class="ant-form-item-required" title="试卷名称">考试时间:</label>
-                        <!-- <el-date-picker
-                            v-model="value6"
-                            type="daterange"
-                            range-separator="-"
-                            start-placeholder="开始日期"
-                            end-placeholder="结束日期">
-                        </el-date-picker> -->
-                    </div>
-                     <div class="ant-form-item-control">
-                        <span class="ant-form-item-children">
-                            <input type="text" id="title" class="ant-input">
-                        </span>
-                    </div>
-                     <div class="ant-form-item-control">
-                        <span class="ant-form-item-children">
-                           <button type="submit" class="ant-btn-primary"><span>创建试卷</span></button>
-                        </span>
-                    </div>
+                        <div class="ant-form-item">
+                            <div class="ant-form-item-label">
+                                <label for="title" class="ant-form-item-required">选择课程:</label>
+                            </div>
+                            <div class="ant-form-item-control">
+                                <el-select class="ant-select-selection-single" v-model="typevalue" placeholder="">
+                                    <el-option 
+                                        v-for="item in course"
+                                        :key="item.typevalue"
+                                        :label="item.label"
+                                        :value="item.typevalue"></el-option>
+                                </el-select>
+                            </div>
+                        </div>
+                        <div class="ant-form-item">
+                            <div class="ant-form-item-label">
+                                <label for="title" class="ant-form-item-required">设置题量:</label>
+                            </div>
+                            <div class="ant-form-item-control">
+                                 <el-input-number class="ant-input-number-input" v-model="number" controls-position="right" @change="handleChange" :min="1" :max="10"></el-input-number>
+                            </div>
+                        </div>
+                        <div class="ant-form-item">
+                            <div class="ant-form-item-label">
+                                <label for="title" class="ant-form-item-required">考试时间:</label>
+                            </div>
+                            <div class="ant-form-item-control">
+                                <el-date-picker v-model="start_time"
+                                    placeholder="开始时间">
+                                </el-date-picker>
+                                <span>-</span>
+                                 <el-date-picker v-model="end_time"
+                                    placeholder="结束时间">
+                                </el-date-picker>
+                            </div>
+                        </div>
+                        <div class="ant-form-item-control">
+                            <button class="ant-btn-primary" @click="createExam(subject_id,number,start_time,end_time)">创建试卷</button>
+                        </div>
+                    </div>                  
                 </div>
             </el-main>
-
         </el-container>
     </div>
 </template>
 <script>
 import PublicHeader from "@/components/publicHeader/index"
+import {mapActions,mapState} from "vuex"
+import examination from '../../store/modules/examination';
 export default {
-    props:{
-
-    },
     components:{
          PublicHeader
     },
+    computed:{
+         ...mapState({
+             types:state=>state.examination.types,
+             course:state=>state.examination.course
+         })
+    },
     data(){
         return {
-             name:"添加考试",
-             input2: '',
-             input21: '',
+            name:"添加考试",
+            pickerOptions1: {
+                disabledDate(time) {
+                    return time.getTime() > Date.now();
+                },
+                shortcuts: [{
+                    text: '今天',
+                    onClick(picker) {
+                    picker.$emit('pick', new Date());
+                    }
+                }, {
+                    text: '昨天',
+                    onClick(picker) {
+                    const date = new Date();
+                    date.setTime(date.getTime() - 3600 * 1000 * 24);
+                    picker.$emit('pick', date);
+                    }
+                }, {
+                    text: '一周前',
+                    onClick(picker) {
+                    const date = new Date();
+                    date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
+                    picker.$emit('pick', date);
+                    }
+                }]
+        },
+        subject_id:"",
+        exam_id:"",
+        title:"",
+        number:4,
+        start_time:"",
+        end_time:"",
+        value:"",
+        typevalue:""
         }
     },
-    computed:{
-
-    },
     methods:{
-
-    },
-    created(){
-
-    },
-    mounted(){
-
+        ...mapActions({
+            getExam:"examination/getExam"
+        }),
+        createExam(subject_id,number,start_time,end_time){
+            window.console.log(subject_id)
+            this.getExam(subject_id,number,start_time,end_time)
+        },
+        handleChange(value) {
+            console.log(value);
+        }
     }
 }
 </script>
@@ -119,114 +143,89 @@ export default {
 .addtest-box{
     background:#f0f2f5;
     width:100%;
-    display: flex;
-    flex-direction: column;
 }
-
 .ant-layout-content{
-    background: rgba(255, 255, 255);
+    background: #fff;
     padding: 24px;
     margin: 0px 0px 20px;
     border-radius: 10px;
-}
-.ant-from{
-    flex:1;
-    background:#fff;
-    margin:0 0 20px;
-    padding:24px;
-}
-.ant-form-item-control{
-    line-height:39.9999px;
-}
-.ant-form-item-children{
-    font-size: 14px;
-    font-variant: tabular-nums;
-    color: rgba(0, 0, 0, 0.65);
-    list-style:none;
-}
-.ant-btn-primary{
-    text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.12);
-    box-shadow: 0 2px 0 rgba(0, 0, 0, 0.045);
-    height:30px;
-    padding: 0 40px;
-    border-radius: 4px;
-    border: 0;
-    font-size: 14px;
-    color: #fff;
-    background: linear-gradient(-90deg,#4e75ff,#0139fd);
-}
-.ant-select{
-    font-size: 14px;
-    font-variant: tabular-nums;
-    line-height: 1.5;
-    color: rgba(0, 0, 0, 0.65);
     box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-    list-style: none;
-    display: inline-block;
+}
+.ant-row{
+    width:40%;
+    margin:0 -3px;
     position: relative;
-    outline: 0;
-}
-.ant-select-selection{
-    outline: none;
-    user-select: none;
-    box-sizing: border-box;
-    display: block;
-    background-color: #fff;
-    border-radius: 2px;
-    border: 1px solid #d9d9d9;
-    border-top-width: 1.02px;
-    transition: all 0.3s cubic-bezier(0.645, 0.045, 0.35)
-}
-.ant-select-selection__rendered{
-    display: block;
-    margin-left: 11px;
-    margin-right: 11px;
-    position: relative;
-    line-height: 30px;
-}
-.ant-select-arrow{
-    display: inline-block;
-    font-style: normal;
-    vertical-align: -0.125em;
-    text-align: center;
-    text-transform: none;
-    line-height: 0;
-    text-rendering: optimizeLegibility;
-    -webkit-font-smoothing: antialiased;
-    position: absolute;
-    top: 50%;
-    right: 11px;
-    line-height: 1;
-    margin-top: -6px;
-    transform-origin: 50% 50%;
-    color: rgba(0, 0, 0, 0.25);
-    font-size: 12px;
-}
-.ant-select-arrow .ant-select-arrow-icon{
+    height:auto;
+    zoom: 1;
     display: block;
 }
-.ant-select-arrow svg{
-    display: inline-block
+.ant-form-item{
+    margin:0 0 24px;
+    height:78.4px;
+}
+.ant-form-item-label{
+    height:39.2px;
+}
+.ant-form-item-required{
+    color:rgba(0,0,0,0.85);
+    font-size:14px;
 }
 .ant-input{
-    font-variant: tabular-nums;
-    box-sizing: border-box;
-    margin: 0;
-    list-style: none;
-    position: relative;
+    width:100%;
+    height:32px;
+    background:#fff;
+    padding:4px 11px;
+}
+.ant-form-item-control{
+    width:100%;
+    height:39.2px;
+    display: flex;
+    align-items: center;
+}
+.ant-form-item-control .el-input__inner{
     display: inline-block;
-    padding: 4px 11px;
-    /* width: 100%; */
-    height: 32px;
-    font-size: 14px;
-    line-height: 1.5;
-    color: rgba(0, 0, 0, 0.65);
-    background-color: #fff;
-    background-image: none;
-    border: 1px solid #d9d9d9;
-    border-radius: 2px;
-    transition: all 0.3s
+}
+.ant-form-item-control span{
+    width:24px;
+    height:100%;
+    color:rgba(0,0,0,.65);
+    font-size:14px;
+    text-align: center;
+    line-height:39.2px;
+}
+.ant-select-selection-single{
+    width:30%;
+    height:32px;
+    background:#fff;
+    border-radius: 4px;
+    border:1px solid #d9d9d9;
+    border-top:1.02px solide #d9d9d9;
+}
+.ant-select-selection-single option{
+    width:100%;
+    overflow: hidden;
+    text-overflow:ellipsis;
+    white-space: nowrap;
+}
+.ant-select-selection_rendered{
+    height:29.6px;
+    margin:0 11px;
+    position: relative;
+}
+
+.ant-input-number-input{
+    width:22%;
+    height:30px;
+    background:#fff;
+    padding:0 11px;
+}
+.ant-btn-primary{
+    height:32px;
+    padding: 0 40px!important;
+    border-radius: 4px!important;
+    border: 0!important;
+    font-size: 14px!important;
+    color: #fff!important;
+    background: linear-gradient(-90deg,#4e75ff,#0139fd)!important;
 }
 </style>
